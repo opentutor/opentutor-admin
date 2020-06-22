ac6b4e23a5018ea3b105350c844e2a50a4c21801
larry kirschner <larrykirschner@gmail.com>
2020-06-22 12:59:40 -0700

    fixes format

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { Session } from "types";
 
-const GRADER_GRAPHQL_ENDPOINT = process.env.GRADER_GRAPHQL_ENDPOINT || "grader/graphql/";
+const GRADER_GRAPHQL_ENDPOINT =
+  process.env.GRADER_GRAPHQL_ENDPOINT || "grader/graphql/";
 
 interface GQLResponse<T> {
   errors: { message: string }[];
@@ -9,8 +10,10 @@ interface GQLResponse<T> {
 }
 
 export async function fetchSessions(): Promise<Session[]> {
-  const result = await axios.post<GQLResponse<Session[]>>(GRADER_GRAPHQL_ENDPOINT, {
-    query: `
+  const result = await axios.post<GQLResponse<Session[]>>(
+    GRADER_GRAPHQL_ENDPOINT,
+    {
+      query: `
             query Sessions {
                 {
                 sessions {
@@ -20,6 +23,7 @@ export async function fetchSessions(): Promise<Session[]> {
                 }
                 }
             `,
-  });
+    }
+  );
   return result.data.data;
 }
