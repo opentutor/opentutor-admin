0cde63eaa3f345b150cf33d489ffa5ad4903ccac
Kayla Carr <whizbon@gmail.com>
2020-10-07 13:48:53 -0700

    Remove weird cypress snapshot tests

--- client/cypress/integration/screenshots-lesson-edit.spec.js
+++ client/cypress/integration/screenshots-lesson-edit.spec.js
@@ -4,94 +4,94 @@ Permission to use, copy, modify, and distribute this software and its documentat
 
 The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 */
-function snapname(n) {
-  return `screenshots-lesson-edit-${n}`;
-}
+// function snapname(n) {
+//   return `screenshots-lesson-edit-${n}`;
+// }
 
-describe("screenshots - lesson edit", () => {
-  beforeEach(() => {
-    cy.viewport(1280, 720);
-  });
+// describe("screenshots - lesson edit", () => {
+//   beforeEach(() => {
+//     cy.viewport(1280, 720);
+//   });
 
-  it("displays lesson form on load", () => {
-    cy.server();
-    cy.route({
-      method: "POST",
-      url: "**/graphql",
-      status: 200,
-      response: {
-        data: {
-          lesson: {
-            lessonId: "lesson",
-            name: "lesson",
-            intro: "intro",
-            question: "question",
-            conclusion: ["conclusion"],
-            expectations: [
-              {
-                expectation: "expectation 1",
-                hints: [
-                  {
-                    text: "hint 1.1",
-                  },
-                ],
-              },
-            ],
-            lastTrainedAt: "",
-            isTrainable: true,
-          },
-        },
-        errors: null,
-      },
-      delay: 10,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).as("getLesson");
-    cy.visit("/lessons/edit?lessonId=lesson");
+//   it("displays lesson form on load", () => {
+//     cy.server();
+//     cy.route({
+//       method: "POST",
+//       url: "**/graphql",
+//       status: 200,
+//       response: {
+//         data: {
+//           lesson: {
+//             lessonId: "lesson",
+//             name: "lesson",
+//             intro: "intro",
+//             question: "question",
+//             conclusion: ["conclusion"],
+//             expectations: [
+//               {
+//                 expectation: "expectation 1",
+//                 hints: [
+//                   {
+//                     text: "hint 1.1",
+//                   },
+//                 ],
+//               },
+//             ],
+//             lastTrainedAt: "",
+//             isTrainable: true,
+//           },
+//         },
+//         errors: null,
+//       },
+//       delay: 10,
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     }).as("getLesson");
+//     cy.visit("/lessons/edit?lessonId=lesson");
 
-    cy.wait("@getLesson");
-    cy.matchImageSnapshot(snapname("displays-lesson-form-on-load"));
-  });
+//     cy.wait("@getLesson");
+//     cy.matchImageSnapshot(snapname("displays-lesson-form-on-load"));
+//   });
 
-  it("displays save button enabled after edits", () => {
-    cy.server();
-    cy.route({
-      method: "POST",
-      url: "**/graphql",
-      status: 200,
-      response: {
-        data: {
-          lesson: {
-            lessonId: "lesson",
-            name: "lesson",
-            question: "question",
-            intro: "intro",
-            conclusion: ["conclusion"],
-            expectations: [
-              {
-                expectation: "expectation 1",
-                hints: [
-                  {
-                    text: "hint 1.1",
-                  },
-                ],
-              },
-            ],
-            lastTrainedAt: "",
-            isTrainable: true,
-          },
-        },
-        errors: null,
-      },
-      delay: 10,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).as("getLesson");
-    cy.visit("/lessons/edit?lessonId=lesson");
-    cy.wait("@getLesson");
-    cy.get("#intro").fill("Hello World");
-    cy.matchImageSnapshot(snapname("displays-save-button-enabled-after-edits"));
-  });
-});
+//   it("displays save button enabled after edits", () => {
+//     cy.server();
+//     cy.route({
+//       method: "POST",
+//       url: "**/graphql",
+//       status: 200,
+//       response: {
+//         data: {
+//           lesson: {
+//             lessonId: "lesson",
+//             name: "lesson",
+//             question: "question",
+//             intro: "intro",
+//             conclusion: ["conclusion"],
+//             expectations: [
+//               {
+//                 expectation: "expectation 1",
+//                 hints: [
+//                   {
+//                     text: "hint 1.1",
+//                   },
+//                 ],
+//               },
+//             ],
+//             lastTrainedAt: "",
+//             isTrainable: true,
+//           },
+//         },
+//         errors: null,
+//       },
+//       delay: 10,
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     }).as("getLesson");
+//     cy.visit("/lessons/edit?lessonId=lesson");
+//     cy.wait("@getLesson");
+//     cy.get("#intro").fill("Hello World");
+//     cy.matchImageSnapshot(snapname("displays-save-button-enabled-after-edits"));
+//   });
+// });
