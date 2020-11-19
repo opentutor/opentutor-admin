742b1e8f14531f84cd7d169412bf2aa4c36de0d8
larry kirschner <larrykirschner@gmail.com>
2020-11-19 12:26:00 -0800

    fixes: batch graphql mocking in cypress to avoid race conditions (#54)

--- cypress/cypress/support/functions.ts
+++ cypress/cypress/support/functions.ts
@@ -4,13 +4,14 @@ export function cySetup(cy) {
 }
 
 export function cyMockGraphQL(cy, query, data) {
-  cy.route2({
-    method: "POST",
-    url: "**/graphql",
-  },
+  cy.route2(
+    {
+      method: "POST",
+      url: "**/graphql",
+    },
     (req) => {
       const g = JSON.parse(req.body);
-      const q = g.query.replace(/\s+/g, ' ').replace("\n", "").trim();
+      const q = g.query.replace(/\s+/g, " ").replace("\n", "").trim();
       if (q.indexOf(`{ ${query}`) !== -1) {
         req.reply({
           body: {
@@ -20,19 +21,75 @@ export function cyMockGraphQL(cy, query, data) {
           headers: {
             "Content-Type": "application/json",
           },
-        })
+        });
       }
     }
   ).as(query);
+}
+
+export interface MockGraphQLQuery {
+  (req: any, grapqlBody: any): void;
+}
+
+export interface MockGraphQLArgs {
+  mocks: MockGraphQLQuery[];
+  alias?: string;
+}
+
+export function cyMockByQueryName(query: string, data: any): MockGraphQLQuery {
+  return (req: any, grapqlBody: any) => {
+    const q = grapqlBody.query.replace(/\s+/g, " ").replace("\n", "").trim();
+    if (q.indexOf(`{ ${query}`) !== -1) {
+      req.reply({
+        body: {
+          data: data,
+          errors: null,
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      req.alias = query;
+    }
+  };
+}
+
+export function cyMockGraphQL2(cy, args: MockGraphQLArgs): void {
+  const r = cy.route2(
+    {
+      method: "POST",
+      url: "**/graphql",
+    },
+    (req) => {
+      const g = JSON.parse(req.body);
+      for (const m of args.mocks) {
+        m(req, g);
+      }
+    }
+  );
+  if (args.alias) {
+    r.as(args.alias);
+  }
 }
 
 export function cyLoginGoogle(cy) {
   cyMockGraphQL(cy, "loginGoogle", {
     loginGoogle: {
       name: "Kayla",
-      email: "kayla@opentutor.com"
+      email: "kayla@opentutor.com",
     },
-  })
+  });
   cy.route("**/config", { GOOGLE_CLIENT_ID: "test" });
   cy.setCookie("accessToken", "accessToken");
+}
+
+export function cyLoginGoogle2(cy): MockGraphQLQuery {
+  cy.route("**/config", { GOOGLE_CLIENT_ID: "test" });
+  cy.setCookie("accessToken", "accessToken");
+  return cyMockByQueryName("loginGoogle", {
+    loginGoogle: {
+      name: "Kayla",
+      email: "kayla@opentutor.com",
+    },
+  });
 }
