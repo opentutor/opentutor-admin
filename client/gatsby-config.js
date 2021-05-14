ac9ba0319a2e3cf29624831963e8ced8ab1b27ce
larry kirschner <larrykirschner@gmail.com>
2021-05-14 12:37:34 -0700

    get rid of usc-specific fav icon

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -7,18 +7,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-env-variables`,
       options: {
         allowList: ["GRAPHQL_ENDPOINT", "CLASSIFIER_ENTRYPOINT"],
