7049b1a25574d8785b5eeca3592bb66e15da451f
Kayla Carr <whizbon@gmail.com>
2021-05-25 16:29:09 -0700

    Link should be from gatsby not react/router

--- client/src/components/nav-bar.tsx
+++ client/src/components/nav-bar.tsx
@@ -4,10 +4,9 @@ Permission to use, copy, modify, and distribute this software and its documentat
 
 The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 */
-import { withPrefix, navigate } from "gatsby";
+import { withPrefix, navigate, Link } from "gatsby";
 import React, { useContext } from "react";
 import { useCookies } from "react-cookie";
-import { Link } from "@reach/router";
 import {
   AppBar,
   Button,
